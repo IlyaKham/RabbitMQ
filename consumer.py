fe7dd0533b0f3b3e002eb798ae6d1a1f693169de
IlyaKham <ilhahamuro@yandex.ru>
2025-08-05 21:47:12 +0400

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -5,7 +5,7 @@ import pika
 credentials = pika.PlainCredentials("admin", "admin123")
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.107', credentials=credentials))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='hello', durable=True, arguments={'x-queue-type': 'quorum'})
 
 
 def callback(ch, method, properties, body):
